7a49c6f9f03f63766b84d001d851f598a8952e06
Ben McPhail <ben@presentcompany.co>
2017-03-13 11:43:44 +1100

    jenkins script - user-data bug and docker

--- AWS/aws-cli-scripts/setup-jenkins/instanceSetup.sh
+++ AWS/aws-cli-scripts/setup-jenkins/instanceSetup.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install jenkins -y
+sudo yum install docker -y
+docker run -p 8080:8080 jenkins
